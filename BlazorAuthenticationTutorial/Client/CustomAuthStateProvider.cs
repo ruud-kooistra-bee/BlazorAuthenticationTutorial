9fc862b68cae6b876a9dc0eb58d96218c8caa7ea
ruud-kooistra-bee <ruud.kooistra.bee@outlook.com>
2022-03-10 11:34:49 +0100

    Add project files.

---
+++ BlazorAuthenticationTutorial/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+
+namespace BlazorAuthenticationTutorial.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            string token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWFyb24gU3dhcnR6IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiTWFzdGVyIEhhY2tlciIsImV4cCI6MTY0Njk5MTA4MX0.yNvdCnbntwFppT-jQxay413hnds3-AD6LmEeeR-Iavs-gcv9uIdy3WuVMy-8cgAPs5WgNvNJPWCsNRn-IuX9xw";
+
+            var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+            //var identity = new ClaimsIdentity();
+
+            var user = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+        }
+
+        private static byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
